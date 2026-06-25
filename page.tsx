4503a184e226d0dd8a86b54ce98f973a7341e477
BoogieBoogatti <124074474+BoogieBoogatti@users.noreply.github.com>
2026-06-25 05:08:08 +0100

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding: '40px', textAlign: 'center', fontFamily: 'sans-serif'}}>
-      <h1>Luminar Genesis 🚀</h1>
-      <p>Site is live after 13 hours of battle!</p>
-    </main>
-  )
-}
