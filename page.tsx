df9c994a8fe3e3a887e3856d8f2093a999102572
BoogieBoogatti <124074474+BoogieBoogatti@users.noreply.github.com>
2026-06-25 04:53:24 +0100

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: '40px', textAlign: 'center', fontFamily: 'sans-serif'}}>
+      <h1>Luminar Genesis 🚀</h1>
+      <p>Site is live after 13 hours of battle!</p>
+    </main>
+  )
+}
