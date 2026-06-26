0de12723686c171deeee9366e71f4c5f69f95052
BoogieBoogatti <124074474+BoogieBoogatti@users.noreply.github.com>
2026-06-26 05:54:54 +0100

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,1 +1,7 @@
-
+    export default function DashboardPage() {
+      return (
+        <main className="flex min-h-screen items-center justify-center">
+          <h1 className="text-4xl font-bold">Welcome to Dashboard ✅</h1>
+        </main>
+      );
+    }
