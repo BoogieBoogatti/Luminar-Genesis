18560c766bac77b8893a4017aa03508b7f9716c0
BoogieBoogatti <124074474+BoogieBoogatti@users.noreply.github.com>
2026-06-26 06:58:57 +0100

    Delete app/dashboard/page.jsx

--- app/dashboard/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      <h1 className="text-3xl font-bold">Welcome to Dashboard</h1>
-    </main>
-  );
-}
